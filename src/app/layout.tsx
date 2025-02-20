e51f4105b7104661c71023599beaf99379ce1869
Esther <estheticallybawo@gmail.com>
2025-02-20 23:21:16 +0100

    UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import { SummarizerProvider } from "@/context/SummerizerContext";
-
-
+import { LanguageDetectionProvider } from "@/context/LanguageDetectionContext";
 
 export const metadata: Metadata = {
   title: "AI Text Processor",
   description: "Built by Esther B",
 };
 
-
-
 export default function RootLayout({
- 
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -21,16 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta 
-         name="chromemeta" 
-        content={process.env.NEXT_PUBLIC_SUMMARIZER_API_TOKEN}
-         data-origin="http://localhost:3000"
-      />
+        <meta
+          name="chromemeta"
+          content={process.env.NEXT_PUBLIC_SUMMARIZER_API_TOKEN}
+          data-origin="http://localhost:3000"
+        />
       </head>
       <body>
         <SummarizerProvider>
-      {children}
-      </SummarizerProvider>
+          <LanguageDetectionProvider>{children}</LanguageDetectionProvider>
+        </SummarizerProvider>
       </body>
     </html>
   );
